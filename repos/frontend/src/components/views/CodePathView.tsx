a8b99e5f72c98a4d0581bcce8aff9692575833e3
Martin Petr <martinpetrnp@gmail.com>
2026-04-07 22:15:07 +0200

    Improve breadcrumbs

--- repos/frontend/src/components/views/CodePathView.tsx
+++ repos/frontend/src/components/views/CodePathView.tsx
@@ -13,17 +13,22 @@ export function CodePathView() {
   const workspace = useWorkspaceState((state) => state.workspace);
 
   const relativePath = path.relative(workspace?.path!, activeTab!);
+  const parts = relativePath.includes("..")
+    ? activeTab?.split("/")
+    : relativePath.split("/");
 
   return (
     <div className="w-full h-6 bg-ctp-mantle flex flex-row gap-2 px-4 items-center text-ctp-overlay2">
-      {relativePath.split("/").map((part, i) => (
-        <React.Fragment key={i}>
-          <CodePathViewItem name={part} />
-          {i < relativePath.split("/").length - 1 && (
-            <PiCaretRight className="w-4 h-4 text-ctp-overlay2" />
-          )}
-        </React.Fragment>
-      ))}
+      {parts
+        ?.filter((p) => p.trim().length > 0)
+        ?.map((part, i) => (
+          <React.Fragment key={i}>
+            <CodePathViewItem name={part} />
+            {i < parts.length - 1 && (
+              <PiCaretRight className="w-4 h-4 text-ctp-overlay2" />
+            )}
+          </React.Fragment>
+        ))}
     </div>
   );
 }
